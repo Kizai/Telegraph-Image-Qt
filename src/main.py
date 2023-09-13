f7b868d790535b8b585ff9ad9f82da73486e08a2
Kizai <41446543+Kizai@users.noreply.github.com>
2023-09-14 00:29:21 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,53 +1,101 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QSystemTrayIcon, QMenu
-from PySide6.QtCore import QUrl
-from PySide6.QtGui import QIcon, QAction, QShortcut, QKeySequence
-from PySide6.QtWebEngineWidgets import QWebEngineView
-from config import IMG_URL, LOGO_PATH
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QPlainTextEdit, QComboBox, \
+	QSystemTrayIcon, QMenu, QAction
+from PyQt5.QtGui import QPixmap, QImage, QClipboard, QIcon
+from PyQt5.QtCore import Qt, QByteArray, QBuffer
+
+import requests
 import sys
 
+API_URL = 'https://telegraph-image-9gk.pages.dev/upload'
+BASE_URL = 'https://telegraph-image-9gk.pages.dev'
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
+LINK_FORMATS = {
+	'URL': '{url}',
+	'Markdown': '![]({url})',
+	'HTML': '<img src="{url}" />',
+	'UBB': '[img]{url}[/img]'
+}
 
-        self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl(IMG_URL))
-        self.setCentralWidget(self.browser)
 
-        # Set the window size to 400*600
-        self.resize(400, 600)
+class ImageUploader(QWidget):
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle('Image Uploader')
 
-        # Create a status bar icon
-        self.tray_icon = QSystemTrayIcon(self)
-        self.tray_icon.setIcon(QIcon(LOGO_PATH))
+		self.tray_icon = QSystemTrayIcon(self)
+		self.tray_icon.setIcon(QIcon("icon.png"))  # Replace "icon.png" with your actual icon file
 
-        # Create context menu
-        quit_action = QAction("Exit", self)
-        quit_action.triggered.connect(app.quit)
+		show_action = QAction("Show", self)
+		quit_action = QAction("Exit", self)
+		hide_action = QAction("Hide", self)
+		show_action.triggered.connect(self.show)
+		hide_action.triggered.connect(self.hide)
+		quit_action.triggered.connect(app.quit)
+		tray_menu = QMenu()
+		tray_menu.addAction(show_action)
+		tray_menu.addAction(hide_action)
+		tray_menu.addAction(quit_action)
+		self.tray_icon.setContextMenu(tray_menu)
+		self.tray_icon.show()
 
-        # Add a new action for navigating to another page
-        manage_action = QAction("Manage", self)
-        manage_action.triggered.connect(self.manage)
+		self.layout = QVBoxLayout()
+		self.setLayout(self.layout)
 
-        tray_menu = QMenu()
-        tray_menu.addAction(manage_action)  # Add the new action
-        tray_menu.addAction(quit_action)
-        self.tray_icon.setContextMenu(tray_menu)
+		self.image_label = QLabel()
+		self.layout.addWidget(self.image_label)
+		self.upload_button = QPushButton('Upload Image')
+		self.layout.addWidget(self.upload_button)
+		self.link_area = QPlainTextEdit()
+		self.link_area.setFixedHeight(50)
+		self.layout.addWidget(self.link_area)
+		self.link_format_combo = QComboBox()
+		self.link_format_combo.addItems(LINK_FORMATS.keys())
+		self.layout.addWidget(self.link_format_combo)
+		self.upload_button.clicked.connect(self.upload_image)
 
-        # Show icon in system tray
-        self.tray_icon.show()
+		self.clear_button = QPushButton('Clear Image')
+		self.layout.addWidget(self.clear_button)
+		self.clear_button.clicked.connect(self.clear_image)
 
-        # Set up a shortcut to refresh the page on Ctrl+R
-        refresh_shortcut = QShortcut(QKeySequence("Ctrl+R"), self)
-        refresh_shortcut.activated.connect(self.browser.reload)
+		self.clipboard = QApplication.clipboard()
+		self.clipboard.dataChanged.connect(self.check_clipboard)
+		self.image = None
 
-    def manage(self):
-        manage_url = IMG_URL + 'admin'  # Replace this with your actual management URL
-        self.browser.setUrl(QUrl(manage_url))
+	def check_clipboard(self):
+		mime_data = self.clipboard.mimeData()
+		if mime_data.hasImage():
+			self.image = QPixmap.fromImage(QImage(mime_data.imageData()))
+			self.image_label.setPixmap(self.image)
+
+	def clear_image(self):
+		self.image_label.clear()
+		self.image = None
+
+	def upload_image(self):
+		try:
+			byte_array = QByteArray()
+			buffer = QBuffer(byte_array)
+			self.image.save(buffer, 'PNG')
+			image_bytes = byte_array.data()
+			response = requests.post(API_URL, files={'file': ('image.png', image_bytes, 'image/png')})
+
+			image_path = response.json()[0]['src']
+
+			image_url = BASE_URL + image_path
+			link_format = self.link_format_combo.currentText()
+			link = LINK_FORMATS[link_format].format(url=image_url)
+			self.link_area.setPlainText(link)
+			self.clipboard.setText(link)
+		except Exception as e:
+			self.link_area.setPlainText(f'Error: {str(e)}')
+
+	def closeEvent(self, event):
+		event.ignore()
+		self.hide()
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+	app = QApplication([])
+	uploader = ImageUploader()
+	uploader.show()
+	sys.exit(app.exec_())
